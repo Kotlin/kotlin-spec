c04a2b71ec8cfba7b9757aeabc2ed37bbcab2089
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-08-16 14:52:47 +0300

    m in processing TODOs

--- scripts/ProcessTODOFilter.kts
+++ scripts/ProcessTODOFilter.kts
@@ -6,6 +6,40 @@
 import ru.spbstu.pandoc.*
 
 class Visitor(val format: String) : PandocVisitor() {
+    override fun visit(iis: List<Inline>, @Suppress("UNUSED") token: Inline?): List<Inline> {
+        val res: MutableList<Inline> = mutableListOf()
+        val transformed = super.visit(iis, token)
+        val it = transformed.iterator()
+        for(e in it) {
+            when {
+                e is Inline.Str && e.text.contains("(TODO") -> {
+                    val interm =
+                            mutableListOf<Inline>(Inline.Str(e.text.substring(e.text.indexOf("(TODO"))))
+                    var tail: Inline? = null
+                    var levels = 0
+                    loop@ for(e in it)  {
+                        when {
+                            e is Inline.Str && e.text.contains("(") ->
+                                ++levels
+                            e is Inline.Str && e.text.contains(")") ->
+                                if(levels == 0) {
+                                    val index = e.text.indexOf(")")
+                                    interm.add(Inline.Str(e.text.substring(0, index + 1)))
+                                    tail = if(index != e.text.lastIndex) Inline.Str(e.text.substring(index + 1)) else null
+                                    break@loop
+                                } else --levels
+                        }
+                        interm.add(e)
+                    }
+                    res.add(Inline.Span(Attr(classes = listOf("TODO")), interm))
+                    if(tail != null) res.add(tail)
+                }
+                else -> res.add(e)
+            }
+        }
+        return res
+    }
+
     override fun visit(b: Block.Para): Block {
         val sup = super.visit(b)
         val first = b.inlines.firstOrNull()
