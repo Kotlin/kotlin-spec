be6f01056662358686d5bd741be65432ed033c30
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2019-10-16 16:43:22 +0300

    Broken link detector

--- docs/scripts/build/buildPdf.sh
+++ docs/scripts/build/buildPdf.sh
@@ -14,6 +14,7 @@ gpp -H ./index.md | pandoc \
 --filter ${FILTERS_DIR}/inlineDiagramFilter.sh \
 --filter ${FILTERS_DIR}/inlineCodeIndenterFilter.sh \
 --filter ${FILTERS_DIR}/mathCleanUpFilter.sh \
+--filter ${FILTERS_DIR}/brokenReferencesReportFilter.sh \
   ${PREAMBLE_OPTIONS} \
   ${COMMON_PANDOC_OPTIONS} \
   ${TOC_PANDOC_OPTIONS} \
