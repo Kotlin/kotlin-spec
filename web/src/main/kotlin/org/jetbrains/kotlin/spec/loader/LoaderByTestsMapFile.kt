1dfc45dc4b45591c3a16ae411a1839014d2f9483
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2020-09-10 15:06:12 +0300

    Introduce sectionsTestMap loading to load only existing testMaps

--- web/src/main/kotlin/org/jetbrains/kotlin/spec/loader/LoaderByTestsMapFile.kt
+++ web/src/main/kotlin/org/jetbrains/kotlin/spec/loader/LoaderByTestsMapFile.kt
@@ -1,31 +1,37 @@
 package org.jetbrains.kotlin.spec.loader
 
-import org.jetbrains.kotlin.spec.entity.SectionTestMap
 import org.jetbrains.kotlin.spec.entity.SpecSection
+import org.jetbrains.kotlin.spec.entity.TestsLoadingInfo
 import org.jetbrains.kotlin.spec.entity.test.SpecTest
 import org.jetbrains.kotlin.spec.entity.test.TestPlace
 import org.jetbrains.kotlin.spec.entity.test.parameters.TestInfo
 import org.jetbrains.kotlin.spec.entity.test.parameters.TestType
 import org.jetbrains.kotlin.spec.entity.test.parameters.testArea.TestArea
+import org.jetbrains.kotlin.spec.loader.GithubTestsLoader.Companion.loadSectionsMapFileFromRawGithub
 import org.jetbrains.kotlin.spec.loader.GithubTestsLoader.Companion.loadTestFileFromRawGithub
 import org.jetbrains.kotlin.spec.loader.GithubTestsLoader.Companion.loadTestMapFileFromRawGithub
+import org.jetbrains.kotlin.spec.loader.GithubTestsLoader.Companion.testAreasToLoad
 import kotlin.js.Promise
 
 
 class LoaderByTestsMapFile : GithubTestsLoader {
-    private val testsMapFilename = "testsMap.json"
 
-    private fun loadTestsMapFile(sectionsPath: String, testAreasToLoad: Array<TestArea>
-    ) = loadTestMapFileFromRawGithub(
-            path = "$sectionsPath/$testsMapFilename",
-            testType = GithubTestsLoader.TestOrigin.SPEC_TEST,
-            testAreasToLoad = testAreasToLoad.asList()
-    )
+    private fun loadTestsMapFile(mainSectionName: String, sectionsPath: String, sectionsMapByTestArea: Map<TestArea, TestsLoadingInfo.Sections>
+    ): Promise<Map<TestArea, TestsLoadingInfo.Tests>> {
+        return loadTestMapFileFromRawGithub(
+                mainSectionName = mainSectionName,
+                path = sectionsPath,
+                testType = GithubTestsLoader.TestOrigin.SPEC_TEST,
+                sectionsMapByTestArea = sectionsMapByTestArea
+        )
+    }
+
+    private fun loadSectionsMapFile() = loadSectionsMapFileFromRawGithub()
 
 
-    private fun getPromisesForTestFilesFromTestMap(testsMapSection: SectionTestMap?, testArea: TestArea): Array<Promise<SpecTest>> {
+    private fun getPromisesForTestFilesFromTestMap(testsMap: TestsLoadingInfo.Tests?, testArea: TestArea): Array<Promise<SpecTest>> {
         val promises = mutableListOf<Promise<SpecTest>>()
-        val testsMap = testsMapSection?.sectionTestMap ?: return promises.toTypedArray()
+        val testsMap = testsMap?.json ?: return promises.toTypedArray()
 
         for ((paragraph, testsByParagraphs) in testsMap.jsonObject) {
             for ((testType, testsByTypes) in testsByParagraphs.jsonObject) {
@@ -43,22 +49,32 @@ class LoaderByTestsMapFile : GithubTestsLoader {
     }
 
 
-    override fun loadTestFiles(sectionName: String, sectionsPath: List<String>, testAreasToLoad: Array<TestArea>
-    ) = loadTestsMapFile(sectionsPath.joinToString("/") + "/" + sectionName, testAreasToLoad)
-            .then { sectionTestMaps ->
+    override fun loadTestFiles(sectionToLoadName: String,
+                               mainSectionPath: String,
+                               sectionsPath: List<String>,
+                               sectionsMapsByTestArea: Map<TestArea, TestsLoadingInfo.Sections>
+    ) = loadTestsMapFile(mainSectionName = mainSectionPath,
+            sectionsPath = when {
+                mainSectionPath == sectionToLoadName && sectionsPath.isEmpty() -> "";
+                sectionsPath.isNotEmpty() -> sectionsPath.joinToString("/") + "/" + sectionToLoadName;
+                else -> sectionToLoadName
+            },
+            sectionsMapByTestArea = sectionsMapsByTestArea)
+            .then { testsMapsByTestArea ->
                 val resultMap = mutableMapOf<TestArea, List<SpecTest>>()
                 Promise.all(testAreasToLoad.asList()
-                        .associateWith { getPromiseForTests(it, sectionTestMaps, resultMap) }
+                        .associateWith { getPromiseForTests(it, testsMapsByTestArea, resultMap) }
                         .values.toTypedArray()
                 ).then { SpecSection(resultMap) }
             }
 
+    fun loadSectionsMapFiles() = loadSectionsMapFile()
 
     private fun getPromiseForTests(
             testArea: TestArea,
-            sectionTestMaps: Map<TestArea, SectionTestMap>,
+            testMaps: Map<TestArea, TestsLoadingInfo.Tests>,
             mapOfTests: MutableMap<TestArea, List<SpecTest>>
     ) = Promise.all(
-            getPromisesForTestFilesFromTestMap(sectionTestMaps[testArea], testArea))
+            getPromisesForTestFilesFromTestMap(testMaps[testArea], testArea))
             .then { mapOfTests[testArea] = it.toList() }
 }
