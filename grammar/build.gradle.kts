59253ddd70a430d2ef2e4992232a82a53d362150
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2021-07-16 19:45:03 +0300

    Update vorpal-research repository

--- grammar/build.gradle.kts
+++ grammar/build.gradle.kts
@@ -21,14 +21,7 @@ val archivePrefix = "kotlin-grammar-parser"
 
 repositories {
     maven {
-        url = URI("https://maven.pkg.github.com/vorpal-research/kotlin-maven")
-        credentials {
-            username = "vorpal-reseacher"
-            password = "\u0031\u0030\u0062\u0037\u0064\u0066\u0031\u0032\u0063\u0064" +
-                    "\u0035\u0034\u0038\u0037\u0034\u0065\u0030\u0034\u0035\u0035" +
-                    "\u0038\u0031\u0063\u0039\u0039\u0062\u0031\u0066\u0032\u0030" +
-                    "\u0038\u0065\u0031\u0061\u0035\u0033\u0065\u0036\u0032\u0038"
-        }
+        url = URI("https://maven.vorpal-research.science")
     }
     mavenCentral()
 }
