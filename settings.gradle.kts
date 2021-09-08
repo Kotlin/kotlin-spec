6b121e738cdf07b41006a17141caba9331dcafb8
Marat Akhin <marat.akhin@jetbrains.com>
2021-09-08 16:38:58 +0300

    Up gradle version to 7.2 and cleanup the build configuration files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,10 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven { setUrl("https://plugins.gradle.org/m2/") }
     }
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
-                "kotlin", "kotlin2js", "kotlin-dce-js" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
-                "org.jetbrains.kotlin.frontend" -> useModule("org.jetbrains.kotlin:kotlin-frontend-plugin:${requested.version}")
                 "at.phatbl.shellexec" -> useModule("gradle.plugin.at.phatbl:shellexec:${requested.version}")
             }
         }
