3ea38743a46a7898f518c286e9223cb615c38fd7
Marat Akhin <marat.akhin@jetbrains.com>
2022-06-22 18:55:09 +0300

    Add processActuals.sh to handle bulk updates to ANTLR grammar in a pseudo-automated way

---
+++ grammar/scripts/processActuals.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TEST_DATA="$1"
+REF="$2"
+
+for actual in `find "${TEST_DATA}" -name "*.actual"`; do
+    old="${actual%.*}"
+    diff "$actual" "$old" | tail -n +2 > "actual_old_diff.tmp"
+    cmp -s "actual_old_diff.tmp" "$REF"
+
+    if [[ $? == 0 ]]; then
+	mv $actual $old
+    else
+	echo "$actual"
+	echo `diff "$actual" "$old"`
+    fi
+done
