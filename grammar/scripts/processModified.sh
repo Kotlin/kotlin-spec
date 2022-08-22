afd49e7289107f7703474fbfb36378b532348b01
Marat Akhin <marat.akhin@jetbrains.com>
2022-08-22 22:51:30 +0200

    Better helper scripts

---
+++ grammar/scripts/processModified.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+REF="$1"
+
+for i in `git status -s | grep "^M.*\.antlrtree.txt$" | cut -d ' ' -f 3`; do
+    git diff --numstat HEAD $i | grep -q "$REF";
+    if [[ $? == 0 ]]; then
+        echo $i;
+    fi
+done
