616292964a7ebb8e06bbef635f71edeb39aae198
Marat Akhin <marat.akhin@jetbrains.com>
2023-07-26 17:49:15 +0200

    [grammar] Update the utility scripts

--- grammar/scripts/processModified.sh
+++ grammar/scripts/processModified.sh
@@ -2,7 +2,7 @@
 
 REF="$1"
 
-for i in `git status -s | grep "^M.*\.antlrtree.txt$" | cut -d ' ' -f 3`; do
+for i in `git status -s | grep "^ M.*\.antlrtree.txt$" | cut -d ' ' -f 3`; do
     git diff --numstat HEAD $i | grep -q "$REF";
     if [[ $? == 0 ]]; then
         echo $i;
